cfea42f35a7ec42921dcf1694b3c44fdd19fbcbf
Jaspreet Singh <jaashy.singh@gmail.com>
2016-10-14 23:55:54 +0200

    --

--- configuration/config.js
+++ configuration/config.js
@@ -4,7 +4,7 @@ module.exports={
 	"facebook_api_secret"	:				"e928ba9662cd00770734fe4be0c0494e",
 	"callback_url"				:				"http://localhost:3000/auth/facebook/callback",
 	"Profile_fields"			:				['id', 'displayName', 'photos', 'email', 'gender','timezone','birthday'],
-	"use_database"				:				"true",
+	"use_database"				:				"false",
 	"host"								:				"localhost",
 	"username"						:				"root",
 	"password"						:				"",
